67041b3c53fd375d7704d34157857aa86ce15b0d
Eduardo Garcia-Castrillon <garciaed@aris-lux.lan>
2019-10-15 12:17:46 +0200

    minor reformat

--- PART_I/data_types/date/task_08.js
+++ PART_I/data_types/date/task_08.js
@@ -16,7 +16,8 @@ let formatDate = date => {
   let year = now.getFullYear().toString().slice(-2);
   let month = ("0" + now.getMonth()).slice(-2);
   let day = ("0" + now.getDate()).slice(-2);
-  return `${day}.${month}.${year} ${now.getHours()}:${now.getMinutes()}`;
+  let formattedNow = `${day}.${month}.${year} ${now.getHours()}:${now.getMinutes()}`;
+  return formattedNow;
 }
 
 describe("Format the relative date", () => {
