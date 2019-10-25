a261d18c1187842f9e12b6735389bfe2943cf652
Eduardo Garcia-Castrillon <edugct@gmail.com>
2019-10-25 11:09:46 +0200

    class-inheritance done

---
+++ PART_I/classes/class-inheritance/task_02.js
@@ -0,0 +1,13 @@
+
+"use strict";
+
+class Rabbit extends Object {
+  constructor(name) {
+    super();
+    this.name = name;
+  }
+}
+
+let rabbit = new Rabbit("Rab");
+
+alert( rabbit.hasOwnProperty('name') ); // true
